7c5f2703e510be5810484233edfc3f58e606ddda
tgonati327 <tgonati327@g.educaand.es>
2024-01-13 18:35:20 +0100

    Creacion del proyecto.

---
+++ src/main/java/daw/Tarea5cTomas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author tomas
+ */
+public class Tarea5cTomas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
